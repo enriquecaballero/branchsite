e4e1a4eea77e3576737c793d35acea3c0ba86388
Enrique Caballero <enrique@visionistinc.com>
2017-05-11 14:59:42 -0400

    chore: Initial commit

---
+++ webpack.config.lib.babel.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-console, import/no-unresolved */
+
+import webpack from "webpack";
+import path from "path";
+import fs from "fs";
+
+export const packageJSON = JSON.parse (
+  fs.readFileSync (path.resolve (path.resolve (__dirname, "package.json")), "utf8")
+);
+
+function createExternalDependencies () {
+  const externals = {};
+  const { peerDependencies, dependencies } = packageJSON;
+  const set = _dependencies => {
+    Object.keys (_dependencies).map (dependency => {
+      externals[dependency] = dependency;
+    });
+  };
+
+  if (dependencies) set (dependencies);
+  if (peerDependencies) set (peerDependencies);
+
+  return externals;
+}
+
+export const webpackLibConfig = {
+  entry: [ path.resolve (__dirname, "src", "index.js") ],
+  output: {
+    path: path.resolve (__dirname, "lib"),
+    filename: "index.js"
+  },
+  externals: createExternalDependencies (),
+  resolve: {
+    extensions: [ "*", ".js" ]
+  },
+  plugins: [
+    new webpack.BannerPlugin ({ banner: "#!/usr/bin/env node", raw: true })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
+
+export default webpackLibConfig;
