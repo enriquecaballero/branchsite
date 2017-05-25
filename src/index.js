f58b58dd6135157791f858640e4fd1353a19a66b
Enrique Caballero <ecaballerojr@icloud.com>
2017-05-25 00:34:07 -0400

    feat: Replaced `commander` with `caporal`
    closes #16

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
-import program from "commander";
+import program from "caporal";
 import hasYarn from "has-yarn";
 import createTasks from "./tasks";
 
 const defaultOptions = {
+  anyBranch: false,
+  noYarn: !hasYarn (),
+  commit: "",
+  noCommit: false,
+  stage: true,
+  noPush: false,
   hook: "build",
   directory: "dist",
-  remote: "origin",
   branch: "gh-pages",
-  yarn: hasYarn (),
-  anyBranch: false,
-  stage: true,
-  commit: true,
-  publish: true
+  remote: "origin"
+};
+
+const launch = (args, options, logger) => {
+  const tasks = createTasks (
+    Object.assign ({}, defaultOptions, JSON.parse (JSON.stringify (options)))
+  );
+  tasks.run ().catch (error => console.error (error));
 };
 
 program
+  .version ("semantic-release")
+  .description (
+    "CLI tool for publishing your static website to a separate branch Edit"
+  )
   .option ("--any-branch", "Allow pushing from any branch")
   .option ("--no-yarn", "Don't use Yarn")
   .option ("-C, --commit <commit>", "Optional commit message")
@@ -28,9 +40,6 @@ program
   )
   .option ("-B, --branch <branch>", "Branch that will be used as subtree")
   .option ("-R, --remote <remote>", "Repository that will be pushed to")
-  .parse (process.argv);
+  .action (launch);
 
-const options = Object.assign ({}, defaultOptions, program);
-const tasks = createTasks (options);
-
-tasks.run ().catch (error => console.error (error));
+program.parse (process.argv);
