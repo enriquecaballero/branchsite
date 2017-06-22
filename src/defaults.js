e3abf3cf550f291e399e6cb847b33f92247b4951
Enrique Caballero <ecaballerojr@icloud.com>
2017-06-22 15:16:14 -0400

    refactor(defaults): Updated to reflect newest args

--- src/defaults.js
+++ src/defaults.js
@@ -3,12 +3,9 @@ import hasYarn from "has-yarn";
 export default {
   anyBranch: false,
   noYarn: !hasYarn (),
-  commit: "",
-  noCommit: false,
-  stage: undefined,
-  noPush: false,
+  noPublish: false,
+  noCleanup: false,
   hook: "build",
   directory: "dist",
-  branch: "gh-pages",
-  remote: "origin"
+  branch: "gh-pages"
 };
