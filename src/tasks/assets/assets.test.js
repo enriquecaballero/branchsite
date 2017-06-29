47d549c58140eeac1fb6c38007d3c3efbc788dad
Enrique Caballero <ecaballerojr@icloud.com>
2017-06-29 11:23:10 -0400

    test: Updated testing for `--skip-install` and `--hook` flags

--- src/tasks/assets/assets.test.js
+++ src/tasks/assets/assets.test.js
@@ -4,51 +4,15 @@ import tasks from "./assets";
 const run = _ => configureTasksForTesting (tasks, _);
 
 describe ("ensures the assets stage works properly", () => {
-  it ("will install dependencies using yarn", () => {
+  it ("will install dependencies using Yarn", () => {
     return run ().then (task => {
       expect (task ("Install dependencies using Yarn").enabled ()).toBeTruthy ();
     });
   });
 
-  it ("will not install dependencies using yarn", () => {
+  it ("will not install dependencies using Yarn", () => {
     return run ("--no-yarn").then (task => {
       expect (task ("Install dependencies using Yarn").enabled ()).toBeFalsy ();
-    });
-  });
-
-  it ("will install dependencies using npm", () => {
-    return run ("--no-yarn").then (task => {
-      expect (task ("Install dependencies using npm").enabled ()).toBeTruthy ();
-    });
-  });
-
-  it ("will not install dependencies using npm", () => {
-    return run ().then (task => {
-      expect (task ("Install dependencies using npm").enabled ()).toBeFalsy ();
-    });
-  });
-
-  it ("will build static assets using yarn", () => {
-    return run ().then (task => {
-      expect (task ("Build static assets using Yarn").enabled ()).toBeTruthy ();
-    });
-  });
-
-  it ("will not build static assets using yarn", () => {
-    return run ("--no-yarn").then (task => {
-      expect (task ("Build static assets using Yarn").enabled ()).toBeFalsy ();
-    });
-  });
-
-  it ("will build static assets using npm", () => {
-    return run ("--no-yarn").then (task => {
-      expect (task ("Build static assets using npm").enabled ()).toBeTruthy ();
-    });
-  });
-
-  it ("will not build static assets using npm", () => {
-    return run ().then (task => {
-      expect (task ("Build static assets using npm").enabled ()).toBeFalsy ();
     });
   });
 
@@ -64,6 +28,18 @@ describe ("ensures the assets stage works properly", () => {
     });
   });
 
+  it ("will install dependencies using npm", () => {
+    return run ("--no-yarn").then (task => {
+      expect (task ("Install dependencies using npm").enabled ()).toBeTruthy ();
+    });
+  });
+
+  it ("will not install dependencies using npm", () => {
+    return run ().then (task => {
+      expect (task ("Install dependencies using npm").enabled ()).toBeFalsy ();
+    });
+  });
+
   it ("will skip installing dependencies using npm", () => {
     return run ("--skip-install --no-yarn").then (task => {
       expect (task ("Install dependencies using npm").skip ()).toBeTruthy ();
@@ -73,6 +49,44 @@ describe ("ensures the assets stage works properly", () => {
   it ("will not skip installing dependencies using npm", () => {
     return run ("--no-yarn").then (task => {
       expect (task ("Install dependencies using npm").skip ()).toBeFalsy ();
+    });
+  });
+
+  it ("will build static assets using Yarn", () => {
+    return run ("--hook npm_build_hook").then (task => {
+      expect (task ("Build static assets using Yarn").enabled ()).toBeTruthy ();
+      expect (task ("Build static assets using Yarn").skip ()).toBeFalsy ();
+    });
+  });
+
+  it ("will disable build static assets using Yarn", () => {
+    return run ("--no-yarn").then (task => {
+      expect (task ("Build static assets using Yarn").enabled ()).toBeFalsy ();
+    });
+  });
+
+  it ("will skip build static assets using Yarn", () => {
+    return run ().then (task => {
+      expect (task ("Build static assets using Yarn").skip ()).toBeTruthy ();
+    });
+  });
+
+  it ("will build static assets using npm", () => {
+    return run ("--no-yarn --hook build").then (task => {
+      expect (task ("Build static assets using npm").enabled ()).toBeTruthy ();
+      expect (task ("Build static assets using npm").skip ()).toBeFalsy ();
+    });
+  });
+
+  it ("will disable build static assets using npm", () => {
+    return run ().then (task => {
+      expect (task ("Build static assets using npm").enabled ()).toBeFalsy ();
+    });
+  });
+
+  it ("will skip build static assets using npm", () => {
+    return run ().then (task => {
+      expect (task ("Build static assets using npm").skip ()).toBeTruthy ();
     });
   });
 });
--- src/tasks/prerequisites/prerequisites.test.js
+++ src/tasks/prerequisites/prerequisites.test.js
@@ -1,12 +1,40 @@
 import { configureTasksForTesting } from "../utils";
-import tasks from "./prerequisites";
-
-const run = _ => configureTasksForTesting (tasks, _);
+import tasks, { prepareWorkspace } from "./prerequisites";
 
 describe ("ensures the prerequisites stage works properly", () => {
+  const run = _ => configureTasksForTesting (tasks, _);
+
   it ("will work on any branch", () => {
     return run ("--any-branch").then (task => {
       expect (task ("Validate current branch").skip ()).toBeTruthy ();
+    });
+  });
+});
+
+describe ("ensures the prerequisites workspace subtasking work properly", () => {
+  const run = _ => configureTasksForTesting (prepareWorkspace, _);
+
+  it ("will delete node_modules", () => {
+    return run ("--skip-install").then (task => {
+      expect (task ("Delete node_modules").skip ()).toBeTruthy ();
+    });
+  });
+
+  it ("will not delete node_modules", () => {
+    return run ().then (task => {
+      expect (task ("Delete node_modules").skip ()).toBeFalsy ();
+    });
+  });
+
+  it ("will delete static assets directory", () => {
+    return run ("--hook npm_build_hook").then (task => {
+      expect (task ("Delete static assets directory").skip ()).toBeFalsy ();
+    });
+  });
+
+  it ("will not delete static assets directory", () => {
+    return run ().then (task => {
+      expect (task ("Delete static assets directory").skip ()).toBeTruthy ();
     });
   });
 });
